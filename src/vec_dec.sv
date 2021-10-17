bb9215c441ff1d71fc87cc05d44839823d9a9ea9
zwystar <92592264+zwystar@users.noreply.github.com>
2021-10-17 23:09:12 +0800

    Update vec_dec.sv

--- src/vec_dec.sv
+++ src/vec_dec.sv
@@ -1,17 +1,14 @@
 module vec_dec
-(
+    #(
+        parameter XLEN = 512,
+        parameter SEW  = 32
+    )
+    (
     input  clk,    // Clock
     input  clk_en, // Clock Enable
     input  rst_n,  // Asynchronous reset active low
     
-    input  [WPORT-1:0] wr_en，
-    input  [WPORT-1:0][XLEN-1:0] wr_be,
-    input  [WPORT-1:0][5-1:0] wr_addr,
-    input  [WPORT-1:0][XLEN-1:0] wr_data,
-    
-    input  [RPORT-1:0] rd_en,
-    input  [RPORT-1:0][5-1:0] rd_addr,
-    output [RPORT-1:0][XLEN-1:0] rd_data,
+    input 
 );
 
 
