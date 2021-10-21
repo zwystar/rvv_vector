242d656e8eea706d4e1fe2e2f05b89d7b7d34a9b
zwystar <92592264+zwystar@users.noreply.github.com>
2021-10-21 11:19:09 +0800

    Update vec_pkg.sv

--- src/vec_pkg.sv
+++ src/vec_pkg.sv
@@ -1,12 +1,14 @@
 package vec_pkg;
-    localparam INSTR_WIDTH = 32;
+    localparam INSTR_BITS = 32;
     localparam XLEN = 32;
+    localparam VLEN = 512;
+    localparam ELEN = 32; 
     // ----------------------
     // Accelerator Interface
     // ----------------------
 
     typedef struct packed {
-      logic [INSTR_WIDTH-1:0]     instr;
+      logic [INSTR_BITS-1:0]      instr;
       logic [XLEN-1:0]            rs1;
       logic [XLEN-1:0]            rs2;
       logic [TRANS_ID_BITS-1:0]   instr_id;
