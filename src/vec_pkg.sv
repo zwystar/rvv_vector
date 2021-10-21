7661f0b11a94e4be3a52ab3ca3927af84b668a59
zwystar <92592264+zwystar@users.noreply.github.com>
2021-10-21 17:25:41 +0800

    Update vec_pkg.sv

--- src/vec_pkg.sv
+++ src/vec_pkg.sv
@@ -3,6 +3,55 @@ package vec_pkg;
     localparam XLEN = 32;
     localparam VLEN = 512;
     localparam ELEN = 32; 
+    
+    // ----------------------
+    // fu op
+    // ----------------------
+    
+    //support all 109 op for fu
+    typedef enum logic [6:0] {
+        VADD, VSUB, VRSUB, 
+        VWADDU, VWSUBU, VWADD, VWSUB, 
+        VZEXT, VSEXT,
+        VADC, VMADC, VSBC, VMSBC,
+        VAND, VOR, VXOR,
+        VSLL, VSRL, VSRA,
+        VNSRL, VNSRA,
+        VMSEQ, VMSNE, VMSLTU, VMSLT, VMSLEU, VMSLE, VMSGTU, VMSGT,
+        VMINU, VMIN, VMAXU, VMAX, 
+        VMUL, VMULH, VMULHU, VMULHSU,
+        //VDIVU, VDIV, VREMU, VREM
+        VWMUL, VWMULU, VWMULSU,
+        VMACC, VNMSAC, VMADD, VNMSUB,
+        VWMACCU, VWMACC, VWMACCSU, VWMACCUS,
+        VMERGE,
+        VMV
+        //all is 50
+        //4 div op is not support
+    } vec_lane_op_e;
+     
+    typedef enum logic [6:0] {
+        VREDSUM, VREDMAXU, VREDMAX, VREDMINU, VREDMIN, VREDAND, VREDOR, VREDXOR,
+        VWREDSUMU, VWREDSUM,
+        VMV,
+        VSLIDEUP, VSLIDEDOWN,VSLIDE1UP, VSLIDE1DOWN,
+        VRGATHER, VRGATHEREI16,
+        VCOMPRESS
+        //all is 18
+    } vec_vsld_op_e
+    
+    typedef enum logic [6:0] {
+        VLD,
+        VST
+        //all is 26
+    } vec_vlsu_op_e
+    
+    typedef enum logic [6:0] {
+        VMAND, VMNAND, VMANDN, VMXOR, VMOR, VMNOR, VMORN, VMXNOR,
+        VCPOP, VFIRST, VMSBF, VMSIF, VMSOF, VIOTA, VID
+        //all is 15
+    } ara_msk_op_e;
+    
     // ----------------------
     // Accelerator Interface
     // ----------------------
