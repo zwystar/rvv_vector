04d22205f42a7be637de65344f59eeb1cee21fdf
zwystar <92592264+zwystar@users.noreply.github.com>
2021-11-17 00:46:50 +0800

    Update dp_stg.sv

--- src/dp/dp_stg.sv
+++ src/dp/dp_stg.sv
@@ -43,6 +43,92 @@ module dp_stage
   
     localparam SN = 8;
   
+    typedef struct packed {
+      logic [SN-1:0]           valid;
+      logic [SN-1:0][SN-1:0]   age;
+      
+      logic [SN-1:0][SN-1:0]   raw;
+      logic [SN-1:0][SN-1:0]   waw;
+      logic [SN-1:0][SN-1:0]   war;
+      logic [SN-1:0][SN-1:0]   vm_war;//dp will updata to 0
+      
+      logic [SN-1:0][SN-1:0]   raw_nblk;//1 can clear the raw when the hzd insn dp
+      logic [SN-1:0][SN-1:0]   waw_nblk;
+      logic [SN-1:0][SN-1:0]   war_nblk;
+      
+      logic [SN-1:0]           vd_st;//1 wait vd write back
+      logic [SN-1:0]           vs_st;//1 wait vs read
+      logic [SN-1:0]           dp_st;//1 wait disp
+      dec_req_t [SN-1:0]       req;
+    } scb_t;
+  logic [SN-1:0] scb_st;
+  ////////////////////////////////////////////////////////
+  //define
+  ////////////////////////////////////////////////////////
+  
+  scb_t scb_dec_cur,scb_dec_next;
+  logic [2:0] scb_dec_id;
+  logic [SN-1:0] dec_vs1_raw,dec_vs2_raw,dec_vd_raw,dec_vm_raw,dec_vd_waw,dec_vs1_war,dec_vs2_war,dec_vd_war,dec_vm_war,dec_raw_nblk,dec_waw_nblk,dec_war_nblk;
+    
+  ////////////////////////////////////////////////////////
+  //dec->scb LOGIC
+  ////////////////////////////////////////////////////////
+  always_comb begin
+      dec_req_ready =~&scb_dec_cur.vd_st;
+      scb_dec_id = (~scb_dec_cur.vd_st) & (scb_dec_cur.vd_st+1'b1);
+      
+      for(int i=0;i<SN;i++)begin
+        dec_vs1_raw[i] = dec_req.use_vs1 & scb_dec_cur.vd_st[i] & (~(dec_req.vs1>scb_dec_cur.req[i].vd_end)|(dec_req.vs1_end<scb_dec_cur.req[i].vd)));
+        dec_vs2_raw[i] = dec_req.use_vs2 & scb_dec_cur.vd_st[i] & (~(dec_req.vs2>scb_dec_cur.req[i].vd_end)|(dec_req.vs2_end<scb_dec_cur.req[i].vd)));
+        dec_vd_raw [i] = dec_req.use_vd  & scb_dec_cur.vd_st[i] & (~(dec_req.vd >scb_dec_cur.req[i].vd_end)|(dec_req.vd_end <scb_dec_cur.req[i].vd)));
+        dec_vm_raw [i] = dec_req.vm & scb_dec_cur.vd_st[i] & (scb_dec_cur.req.vd[i]=='d0);
+        
+        dec_vd_waw [i] = dec_req.wr_vd & scb_dec_cur.vd_st[i] & (~(dec_req.vd >scb_dec_cur.req[i].vd_end)|(dec_req.vd_end <scb_dec_cur.req[i].vd)));
+        
+        dec_vs1_war[i] = scb_dec_cur.vs_st[i] & dec_req.wr_vd & (~(dec_req.vd >scb_dec_cur.req[i].vs1_end)|(dec_req.vd_end <scb_dec_cur.req[i].vs1)));
+        dec_vs2_war[i] = scb_dec_cur.vs_st[i] & dec_req.wr_vd & (~(dec_req.vd >scb_dec_cur.req[i].vs2_end)|(dec_req.vd_end <scb_dec_cur.req[i].vs2)));
+        dec_vd_war [i] = scb_dec_cur.vs_st[i] & dec_req.wr_vd & (~(dec_req.vd >scb_dec_cur.req[i].vd_end )|(dec_req.vd_end <scb_dec_cur.req[i].vd)));
+        dec_vm_war [i] = scb_dec_cur.dp_st[i] & dec_req.wr_vd & scb_dec_cur.req[i].vm & (dec_req.vd=='d0);
+        
+        dec_raw_nblk[i]= dec_req.nblk & scb_dec_cur.req[i].nblk & (dec_req.lmul==scb_dec_cur.req[i].lmul) & 
+          (dec_vs1_raw[i] ? (dec_req.vs1_sew==scb_dec_cur.req[i].vd_sew):1'b0) &
+          (dec_vs2_raw[i] ? (dec_req.vs2_sew==scb_dec_cur.req[i].vd_sew):1'b0) &
+          (dec_vd_raw [i] ? (dec_req.vd_sew ==scb_dec_cur.req[i].vd_sew):1'b0) ;
+        dec_waw_nblk[i]= dec_req.nblk & scb_dec_cur.req[i].nblk & (dec_req.lmul==scb_dec_cur.req[i].lmul) & (dec_req.vd_sew==scb_dec_cur.req[i].vd_sew);
+        dec_war_nblk[i]= dec_req.nblk & scb_dec_cur.req[i].nblk & (dec_req.lmul==scb_dec_cur.req[i].lmul) & 
+          (dec_vs1_war[i] ? (dec_req.vd_sew==scb_dec_cur.req[i].vs1_sew):1'b0) &
+          (dec_vs2_war[i] ? (dec_req.vd_sew==scb_dec_cur.req[i].vs2_sew):1'b0) &
+          (dec_vd_war[i]  ? (dec_req.vd_sew==scb_dec_cur.req[i].vd_sew):1'b0) ;
+      end
+      
+      scb_dec_next = scb_dec_cur;
+      scb_dec_next.valid[scb_dec_id] = 1'b1;
+      scb_dec_next.age[scb_dec_id] = scb_dec_cur.valid;
+      scb_dec_next.raw[scb_dec_id] = dec_vs1_raw | dec_vs2_raw | dec_vd_raw | dec_vm_raw;
+      scb_dec_next.waw[scb_dec_id] = dec_vd_waw;
+      scb_dec_next.war[scb_dec_id] = dec_vs1_war | dec_vs2_war | dec_vd_war | dec_vm_war;
+      scb_dec_next.raw_nblk[scb_dec_id] = dec_raw_nblk;
+      scb_dec_next.waw_nblk[scb_dec_id] = dec_waw_nblk;
+      scb_dec_next.war_nblk[scb_dec_id] = dec_war_nblk;
+      scb_dec_next.vd_st[scb_dec_id] = dec_req.wr_vd;
+      scb_dec_next.vs_st[scb_dec_id] = 1'b1;
+      scb_dec_next.dp_st[scb_dec_id] = 1'b1;
+      scb_dec_next.req[scb_dec_id] = dec_req;
+  end
+  ////////////////////////////////////////////////////////
+  //issue
+  ////////////////////////////////////////////////////////
+  always_comb begin
+    
+    
+  end
+  
+  
+  
+  
+  
+  
+  
     genvar ii;
   /////////////////////////////////////////////////////////////////////////////////////////////////
   //score board req write
